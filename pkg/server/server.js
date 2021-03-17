40ab30ef698f2f73aed8542c2fe9cd3e289499c0
Odin Hørthe Omdal <odin.omdal@gmail.com>
2021-03-17 20:44:35 +0100

    server: Use all CPUs by using node cluster

--- pkg/server/server.js
+++ pkg/server/server.js
@@ -1,46 +1,63 @@
+const cluster = require("cluster");
+
 const express = require("express");
 const { postgraphile } = require("postgraphile");
 const pg_simplify_inflector = require("@graphile-contrib/pg-simplify-inflector");
+const cCPUs = require("os").cpus().length;
+
 const { DEV, DATABASE_URL, OWNER_DATABASE_URL } = require("./config.js");
 
-const app = express();
+if (cluster.isMaster) {
+  for (let i = 0; i < cCPUs; i++) {
+    cluster.fork();
+  }
 
-app.use(
-  postgraphile(DATABASE_URL, "roiheimen", {
-    //pgSettings(req) {
-    //  console.log("req", req.query);
-    //  if ('person' in req.query) return { role: "roiheimen_person" };
-    //  return { role: "roiheimen_anonymous", };
-    //},
-    pgDefaultRole: "roiheimen_anonymous",
-    jwtSecret: "secret_kitten",
-    jwtPgTypeIdentifier: "roiheimen.jwt_token",
-    ownerConnectionString: OWNER_DATABASE_URL,
+  cluster.on("online", function(worker) {
+    console.log("Worker " + worker.process.pid + " is online.");
+  });
+  cluster.on("exit", function(worker, code, signal) {
+    console.log("worker " + worker.process.pid + " died.");
+  });
+} else {
+  const app = express();
 
-    disableQueryLog: !DEV, // querylog is slow
-    enhanceGraphiql: DEV,
-    enableCors: DEV,
-    extendedErrors: DEV ? ["hint", "detail", "errcode"] : ["errcode"],
-    graphiql: DEV,
-    watchPg: DEV,
+  app.use(
+    postgraphile(DATABASE_URL, "roiheimen", {
+      //pgSettings(req) {
+      //  console.log("req", req.query);
+      //  if ('person' in req.query) return { role: "roiheimen_person" };
+      //  return { role: "roiheimen_anonymous", };
+      //},
+      pgDefaultRole: "roiheimen_anonymous",
+      jwtSecret: "secret_kitten",
+      jwtPgTypeIdentifier: "roiheimen.jwt_token",
+      ownerConnectionString: OWNER_DATABASE_URL,
 
-    appendPlugins: [pg_simplify_inflector, require("@graphile/subscriptions-lds").default],
-    live: true,
-    dynamicJson: true,
-    enableQueryBatching: true,
-    ignoreIndexes: false,
-    ignoreRBAC: false,
-    legacyRelations: "omit",
-    retryOnInitFail: true,
-    setofFunctionsContainNulls: false,
-    subscriptions: true,
-  })
-);
+      disableQueryLog: !DEV, // querylog is slow
+      enhanceGraphiql: DEV,
+      enableCors: DEV,
+      extendedErrors: DEV ? ["hint", "detail", "errcode"] : ["errcode"],
+      graphiql: DEV,
+      watchPg: DEV,
 
-const port = process.env.PORT || 3000;
-app.listen(port);
+      appendPlugins: [pg_simplify_inflector, require("@graphile/subscriptions-lds").default],
+      live: true,
+      dynamicJson: true,
+      enableQueryBatching: true,
+      ignoreIndexes: false,
+      ignoreRBAC: false,
+      legacyRelations: "omit",
+      retryOnInitFail: true,
+      setofFunctionsContainNulls: false,
+      subscriptions: true,
+    })
+  );
 
-console.log(`Listening on http://localhost:${port}${DEV ? " (dev mode)" : ""}`);
-if (DEV) {
-  console.log(`Graphiql at http://localhost:${port}/graphiql`);
+  const port = process.env.PORT || 3000;
+  app.listen(port);
+
+  console.log(`Listening on http://localhost:${port}${DEV ? " (dev mode)" : ""}`);
+  if (DEV) {
+    console.log(`Graphiql at http://localhost:${port}/graphiql`);
+  }
 }
